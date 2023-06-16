4a7dcdf379d7a631c27cfaef12f873eda12c45bd
Peter <hansburon@web.de>
2023-06-16 15:46:09 +0200

    add variables to main.js add eventlistener

---
+++ main.js
@@ -0,0 +1,27 @@
+const keys = document.querySelectorAll('.key');
+const displayInput = document.querySelector('.display .input');
+const displayOutput = document.querySelector('.display .output');
+
+
+let input = "";
+
+for (let key of keys) {
+    const value = key.dataset.key;
+
+    key.addEventListener('click', () => {
+        if (value == "clear") {
+            input = "";
+            displayInput.innerHTML = "";
+            displayOutput.innerHTML = "";
+        } else if (value == "backspace") {
+            input = input.slice(0, -1);
+            displayInput.innerHTML = input;
+        } else if (value == "=") {
+            let result = eval(input);
+
+            displayOutput.innerHTML = result;
+        }
+
+    }
+    )
+}
