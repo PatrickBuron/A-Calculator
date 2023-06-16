a742f225d205e6f3f0dca8538f8f77ea7c98d3d6
Peter <hansburon@web.de>
2023-06-16 19:16:27 +0200

    add new function for clean input in main.js

--- main.js
+++ main.js
@@ -13,19 +13,41 @@ for (let key of keys) {
             input = "";
             displayInput.innerHTML = "";
             displayOutput.innerHTML = "";
-        } else if (value == "backspace") {
-            input = input.slice(0, -1);
-            displayInput.innerHTML = input;
+            // } else if (value == "backspace") {
+            //     input = input.slice(0, -1);
+            //     displayInput.innerHTML = input;
         } else if (value == "=") {
             let result = eval(input);
 
             displayOutput.innerHTML = result;
-            displayInput.innerHTML = input;
+            displayInput.innerHTML = CleanInput(input);
         } else {
             input += value;
-            displayInput.innerHTML = input;
+            displayInput.innerHTML = CleanInput(input);
         }
 
     }
     )
+}
+
+
+function CleanInput(input) {
+    let input_array = input.split("");
+    let input_array_length = input_array.length;
+
+    for (let i = 0; i < input_array_length; i++) {
+        if (input_array[i] == "*") {
+            input_array[i] = ` <span class="operator">x</span> `;
+        } else if (input_array[i] == "/") {
+            input_array[i] = ` <span class="operator">&divide</span> `;
+        } else if (input_array[i] == "+") {
+            input_array[i] = ` <span class="operator">+</span> `;
+        } else if (input_array[i] == "-") {
+            input_array[i] = ` <span class="operator">-</span> `;
+        } else if (input_array[i] == "%") {
+            input_array[i] = ` <span class="operator">%</span> `;
+        }
+
+    }
+    return input_array.join("");
 }
