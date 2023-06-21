6570e31a83ede391e45eb14211cff7142582e892
Peter <hansburon@web.de>
2023-06-21 15:01:19 +0200

    add function for checking the length of the displayoutput to use a smaller font size when the string  has more than 10 numbers

--- main.js
+++ main.js
@@ -1,70 +1,4 @@
-// const keys = document.querySelectorAll('.key');
-// const displayInput = document.querySelector('.display .input');
-// const displayOutput = document.querySelector('.display .output');
 
-
-// let input = "";
-
-// for (let key of keys) {
-//     const value = key.dataset.key;
-
-//     key.addEventListener('click', () => {
-//         if (value == "clear") {
-//             input = "";
-//             displayInput.innerHTML = "";
-//             displayOutput.innerHTML = "";
-//             // changeFontSize(32);
-//         } else if (value == "=") {
-//             let result = eval(input);
-//             // round number to 3 decimal points
-//             result = Math.round(result * 1000) / 1000;
-//             check_result_length(result);
-
-//             displayOutput.innerHTML = result;
-//             displayInput.innerHTML = CleanInput(input);
-//         } else {
-//             input += value;
-//             displayInput.innerHTML = CleanInput(input);
-//         }
-
-//     }
-//     )
-// }
-
-
-// function CleanInput(input) {
-//     let input_array = input.split("");
-//     let input_array_length = input_array.length;
-
-//     for (let i = 0; i < input_array_length; i++) {
-//         if (input_array[i] == "*") {
-//             input_array[i] = ` <span class="operator">x</span> `;
-//         } else if (input_array[i] == "/") {
-//             input_array[i] = ` <span class="operator">/</span> `;
-//         } else if (input_array[i] == "+") {
-//             input_array[i] = ` <span class="operator">+</span> `;
-//         } else if (input_array[i] == "-") {
-//             input_array[i] = ` <span class="operator">-</span> `;
-//         } else if (input_array[i] == "%") {
-//             input_array[i] = ` <span class="operator">%</span> `;
-//         }
-
-//     }
-//     return input_array.join("");
-// }
-
-// function check_result_length(result) {
-//     if (Number(result) < 999999999999) {
-//         displayOutput.style.fontSize = "32px";
-//         displayOutput.style.paddingTop = "10px";
-//     } else if (Number(result) > 999999999999) {
-//         displayOutput.style.fontSize = "20px";
-//         displayOutput.style.paddingTop = "20px";
-//     }
-// }
-
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! N E W   JAVASCRIPT  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
@@ -110,10 +44,10 @@ class Calculator {
             case '-':
                 computation = prev - current
                 break
-            case '*':
+            case 'x':
                 computation = prev * current
                 break
-            case '÷':
+            case '/':
                 computation = prev / current
                 break
             default:
@@ -126,6 +60,7 @@ class Calculator {
 
     getDisplayNumber(number) {
         const stringNumber = number.toString()
+        check_stringNumber_length(stringNumber);
         const integerDigits = parseFloat(stringNumber.split('.')[0])
         const decimalDigits = stringNumber.split('.')[1]
         let integerDisplay
@@ -139,6 +74,7 @@ class Calculator {
         } else {
             return integerDisplay
         }
+
     }
 
     updateDisplay() {
@@ -150,6 +86,7 @@ class Calculator {
         } else {
             this.previousOperandTextElement.innerText = ''
         }
+
     }
 }
 
@@ -192,3 +129,25 @@ deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
 })
+
+
+
+
+
+function check_stringNumber_length(result) {
+
+    if (result.length <= 10) {
+        currentOperandTextElement.style.fontSize = "32px";
+
+    } else if (result.length > 10) {
+        currentOperandTextElement.style.fontSize = "20px";
+
+    }
+}
+
+
+
+
+
+
+
