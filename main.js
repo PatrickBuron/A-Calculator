c0a546c6f73b38d3d364f03bfad0e1fdc7629f3c
Peter <hansburon@web.de>
2023-06-21 16:01:50 +0200

    add comments to the js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 
+// For this java script code i followed a tutorial on youtube ( https://youtu.be/j59qQ7YWLxw } and added a new function at the end of the code to fix the bug with the overflowing number on the display.
+
+
 
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
@@ -7,20 +10,31 @@ class Calculator {
         this.clear()
     }
 
+
+    // this following function is to clear/delete the complet display 
+
     clear() {
         this.currentOperand = ''
         this.previousOperand = ''
         this.operation = undefined
     }
 
+    // this following function is to delete the last character of the string on the display
+
     delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
+
+
+    // this following function is to return all the pressed numbers as a string on the display
 
     appendNumber(number) {
         if (number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
     }
+
+
+    // this function is to ....
 
     chooseOperation(operation) {
         if (this.currentOperand === '') return
@@ -31,6 +45,9 @@ class Calculator {
         this.previousOperand = this.currentOperand
         this.currentOperand = ''
     }
+
+
+    //  this function adds tasks to the operatores 
 
     compute() {
         let computation
@@ -57,6 +74,7 @@ class Calculator {
         this.operation = undefined
         this.previousOperand = ''
     }
+    //    this following function is to turns the number into a string and adds decimal numbers 
 
     getDisplayNumber(number) {
         const stringNumber = number.toString()
@@ -76,6 +94,9 @@ class Calculator {
         }
 
     }
+
+
+    // thsi function is to update the display .the current number changes into the previous number
 
     updateDisplay() {
         this.currentOperandTextElement.innerText =
@@ -100,6 +121,7 @@ const previousOperandTextElement = document.querySelector('[data-previous-operan
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+// the follwoing function adds a click event to each number button on the claculator and returns the inner text element eventually updates the display
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -108,6 +130,9 @@ numberButtons.forEach(button => {
     })
 })
 
+
+// the follwoing function adds a click event to each operation button on the claculator and returns the inner text element eventually updates the display
+
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.chooseOperation(button.innerText)
@@ -115,15 +140,23 @@ operationButtons.forEach(button => {
     })
 })
 
+// the follwoing function adds a click event to the equal button on the claculator and returns the inner text element eventually , computes and updates the display , after running the clear function
+
 equalsButton.addEventListener('click', button => {
     calculator.compute()
     calculator.updateDisplay()
 })
 
+
+// the follwoing function adds a click event to clear button on the claculator  eventually updates the display after running the clear function
+
 allClearButton.addEventListener('click', button => {
     calculator.clear()
     calculator.updateDisplay()
 })
+
+
+// the follwoing function adds a click event to delete button on the claculator , eventually updates the display after running the clear function
 
 deleteButton.addEventListener('click', button => {
     calculator.delete()
@@ -131,7 +164,9 @@ deleteButton.addEventListener('click', button => {
 })
 
 
+// The check_stringNumber_length is a selfmade function and is not part of the tutorial !!!!!
 
+// this function checks how long the string is and chooses the beter font size  to prevent a text overflow at the display element 
 
 
 function check_stringNumber_length(result) {
